2dc3ad50b92fa694c1dd51c51060251ae37ec52b
yactouat <yactouat@hotmail.com>
2023-05-14 20:47:22 +0200

    more vertical margins on app' section

--- components/app-section/app-section.tsx
+++ components/app-section/app-section.tsx
@@ -5,7 +5,7 @@ export default function AppSection({ children, id, title }: AppSectionProps) {
   return (
     <section
       id={id}
-      className="p-3 my-3 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height"
+      className="p-3 my-4 scroll-mt-20 widescreen:section-min-height tallscreen:section-min-height"
     >
       {title != "" && (
         <h2 className="text-4xl font-bold text-center sm:text-5xl mb-6 text-slate-900 dark:text-white">
